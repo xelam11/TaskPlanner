18c1949395705b679d260a5dd6f903bcb0c8e2d9
Александр Медведев <xelam11@yandex.ru>
2021-09-12 18:49:59 +0300

    create action 'favorite' in BoardViewSet

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,9 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-from .models import Board, List
+from .models import Board, List, Favorite
 from .permissions import IsStaffOrAuthorOrAuthenticated
 from .serializers import BoardSerializer, ListSerializer
 
@@ -28,3 +31,33 @@ class BoardViewSet(viewsets.ModelViewSet):
             return Board.objects.all()
 
         return Board.objects.filter(author=current_user)
+
+    @action(detail=True, methods=['POST', 'DELETE'])
+    def favorite(self, request, **kwargs):
+        user = request.user
+        board = get_object_or_404(Board, id=kwargs.get('pk'))
+
+        if request.method == 'POST':
+            _, is_created = Favorite.objects.get_or_create(user=user,
+                                                           board=board)
+
+            if not is_created:
+                return Response({
+                    'status': 'error',
+                    'message': 'Вы уже добавили данную доску в избранное!'},
+                    status=status.HTTP_400_BAD_REQUEST)
+
+            return Response(status=status.HTTP_201_CREATED)
+
+        if request.method == 'DELETE':
+            count, _ = Favorite.objects.filter(
+                user=user,
+                board=board).delete()
+
+            if count == 0:
+                return Response({
+                    'status': 'error',
+                    'message': 'Вы уже далили данную доску из избранных!'},
+                    status=status.HTTP_400_BAD_REQUEST)
+
+            return Response(status=status.HTTP_204_NO_CONTENT)

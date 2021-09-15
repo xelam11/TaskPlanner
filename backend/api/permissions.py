f6238286b1b9c8a938dc0ef8c9339dbb196e7056
Александр Медведев <xelam11@yandex.ru>
2021-09-15 12:47:53 +0300

    correct permission for BoardViewSet: participants can't use PUT, PATCH and DELETE methods

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -10,10 +10,16 @@ class IsStaffOrAuthorOrAuthenticated(permissions.BasePermission):
         return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        if (request.method in ['GET', 'PUT', 'PATCH', 'DELETE'] and
-                request.user.is_authenticated):
+
+        if request.method == 'GET' and request.user.is_authenticated:
             return (request.user == obj.author or
                     request.user in obj.participants.all() or
+                    request.user.is_superuser or
+                    request.user.is_staff
+                    )
+        if (request.method in ['PUT', 'PATCH', 'DELETE'] and
+                request.user.is_authenticated):
+            return (request.user == obj.author or
                     request.user.is_superuser or
                     request.user.is_staff
                     )

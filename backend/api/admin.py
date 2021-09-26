330950066468e85cb86ac668b7278c64f489db32
Александр Медведев <xelam11@yandex.ru>
2021-09-26 14:23:53 +0300

    create model ParticipantRequest

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Board, Favorite, List
+from .models import Board, Favorite, List, ParticipantRequest
 
 
 class BoardAdmin(admin.ModelAdmin):
@@ -14,3 +14,4 @@ class ListAdmin(admin.ModelAdmin):
 admin.site.register(Board, BoardAdmin)
 admin.site.register(Favorite)
 admin.site.register(List, ListAdmin)
+admin.site.register(ParticipantRequest)
--- backend/api/models.py
+++ backend/api/models.py
@@ -83,6 +83,29 @@ class Favorite(models.Model):
     def __str__(self):
         return (f'Пользователь: {self.user}, '
                 f'избранные доски: {self.board.name}')
+
+
+class ParticipantRequest(models.Model):
+    board = models.ForeignKey(Board,
+                              on_delete=models.CASCADE,
+                              related_name='requesting_board',
+                              verbose_name='Доска',
+                              )
+    participant = models.ForeignKey(CustomUser,
+                                    on_delete=models.CASCADE,
+                                    related_name='requested_participant',
+                                    verbose_name='Участник')
+
+    class Meta:
+        verbose_name = 'Запрос'
+        verbose_name_plural = 'Запросы'
+        constraints = [models.UniqueConstraint(
+            fields=['board', 'participant'],
+            name='unique_requests')]
+
+    def __str__(self):
+        return (f'Доска: {self.board}, '
+                f'запрашиваемый пользователь: {self.participant}')
 #
 #
 # class Tag(models.Model):

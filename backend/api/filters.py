2f4be8d339f6ca24eef646f0dcd4ce5bc752d35a
Александр Медведев <xelam11@yandex.ru>
2021-10-16 13:33:23 +0300

    feat: add field 'name' to BoardFilter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,16 +1,17 @@
 import django_filters as filters
 
-from .models import Board
+from .models import Board, ParticipantInBoard
 
 
 class BoardFilter(filters.FilterSet):
     is_favored = filters.BooleanFilter(method='get_is_favored')
     is_author = filters.BooleanFilter(method='get_is_author')
     is_participant = filters.BooleanFilter(method='get_is_participant')
+    name = filters.CharFilter(field_name='name', lookup_expr='contains')
 
     class Meta:
         model = Board
-        fields = ('is_favored', 'is_author', 'is_participant')
+        fields = ('is_favored', 'is_author', 'is_participant', 'name')
 
     def get_is_favored(self, queryset, name, value):
         user = self.request.user
@@ -35,3 +36,19 @@ class BoardFilter(filters.FilterSet):
             return queryset.filter(participants__id=user.id)
 
         return queryset.exclude(participants__id=user.id)
+
+
+# class ParticipantsFilter(filters.FilterSet):
+#     is_moderator = filters.BooleanFilter(method='get_is_moderator')
+#
+#     class Meta:
+#         model = ParticipantInBoard
+#         fields = ('is_moderator', )
+#
+#     def get_is_moderator(self, queryset, name, value):
+#         breakpoint()
+#         if value:
+#             return queryset.filter(is_moderator=True)
+#
+#         else:
+#             return queryset.filter(is_moderator=False)
